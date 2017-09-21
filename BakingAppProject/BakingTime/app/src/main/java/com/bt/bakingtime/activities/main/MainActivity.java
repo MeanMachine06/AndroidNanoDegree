d14a5759b88d0755657a42ff5ac71883fd15dcb1
Aditya <im.aditya0689@gmail.com>
2017-09-21 14:03:13 +0530

    Added error message to the MainActivity (if the recipe list is empty)

--- BakingAppProject/BakingTime/app/src/main/java/com/bt/bakingtime/activities/main/MainActivity.java
+++ BakingAppProject/BakingTime/app/src/main/java/com/bt/bakingtime/activities/main/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import com.bt.bakingtime.R;
 import com.bt.bakingtime.activities.recipedetaillist.RecipeDetailListActivity;
@@ -31,6 +33,7 @@ public class MainActivity extends AppCompatActivity
     private LinearLayoutManager mLinearLayoutManager;
     private RecipeListAdapter mRecipeListAdapter;
     private Parcelable mRecipeListState;
+    private TextView mErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -43,6 +46,8 @@ public class MainActivity extends AppCompatActivity
                                      .build());
 
         setContentView(R.layout.activity_main);
+
+        mErrorMessage = (TextView) this.findViewById(R.id.tv_error_message);
 
         getRecipiesData();
         mRecipeListAdapter = new RecipeListAdapter(this, mRecipiesData);
@@ -109,6 +114,17 @@ public class MainActivity extends AppCompatActivity
             protected void onPostExecute(ArrayList<Recipe> recipes)
             {
                 super.onPostExecute(recipes);
+
+                if(recipes.size() == 0)
+                {
+                    mErrorMessage.setText(R.string.error_message);
+                    mErrorMessage.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    mErrorMessage.setVisibility(View.GONE);
+                }
+
                 mRecipeListAdapter.setRecipesData(recipes);
 
                 if(mRecipeListState != null)
